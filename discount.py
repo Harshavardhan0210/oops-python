0978c20aa783cd9e99ab1df74b62a92404da7886
Harshavardhan M M <mmharshavardhan5@gmail.com>
2026-06-15 18:32:45 +0530

    Update discount.py

--- discount.py
+++ discount.py
@@ -4,3 +4,7 @@ def purchase(product,price):
     print("Total price of ", product , "is", str(tot))
     
 purchase("iphone",70000)
+
+//Consider the below code which allows you to purchase different products. All products have discount of 10%.
+
+Run the below code and observe the output.
